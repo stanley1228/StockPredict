941baa1b46224235213824f40d420c5015d3bae2
stanley1228 <stanleyfreeze@gmail.com>
2018-05-19 21:51:50 +0800

    change to data_helper_no_list but etf problem

--- StockPredict.py
+++ StockPredict.py
@@ -6,6 +6,7 @@ from sklearn import preprocessing
 from keras.models import Sequential
 from keras.layers.core import Dense,Dropout,Activation
 from keras.layers.recurrent  import LSTM
+from keras.models import Model
 # import keras
 import matplotlib.pyplot as plt 
 
@@ -51,10 +52,10 @@ def data_helper(tsharepdf,tetfpdf,day_frame):
 
         #x train data
         x_train_list.append(OneStockFrameData[:int(number_train)])   
-
+        
         #x test data
         x_test_list.append(OneStockFrameData[int(number_train):])
-
+       
     '''ETF''' 
     ETFGroup=tetfpdf.groupby('No')
     
@@ -85,6 +86,67 @@ def data_helper(tsharepdf,tetfpdf,day_frame):
    
     return [x_train_list, y_train_list, x_test_list, y_test_list]
 
+def data_helper_no_list(tsharepdf,tetfpdf,day_frame):
+    
+    number_features=5
+    pred_days=5 #predict future 5 days
+
+    '''share'''
+    
+    #find how many days of the data
+    StocksGroup=tsharepdf.groupby('No')
+    Group1101=StocksGroup.get_group(1101)
+    data_days=len(Group1101)
+
+    x_train_list=[]
+    x_test_list=[]
+    AllFrameData=np.array()
+    for index in range(data_days-(day_frame+pred_days)+1):
+        DayFrameData=np.array()
+        for stock_no,OneStockdf in StocksGroup:
+            if len(OneStockdf) != data_days:
+                continue
+            OneStock_mx=OneStockdf.as_matrix()
+            DayFrameData.append(OneStock_mx[index:index+day_frame,2:8]) #"No","Date","Name","Open","High","Low","Close","Volume"     
+    AllFrameData.append(DayFrameData)    
+                 
+    number_train=round(0.5*AllFrameData.shape[0])
+    #x train data
+    x_train_list.append(AllFrameData[:int(number_train)])   
+    
+    #x test data
+    x_test_list.append(AllFrameData[int(number_train):])
+       
+    '''ETF''' 
+    ETFGroup=tetfpdf.groupby('No')
+    
+    y_train_list=[]
+    y_test_list=[]
+
+    for index in range(day_frame,OneETF_mx.shape[0]-pred_days+1):
+        OneETFFrameData.append(OneETF_mx[index:index+pred_days,5]) #"No","Date","Name","Open","High","Low","Close","Volume"     
+
+    for ETF_no,OneETFdf in ETFGroup:
+        DayFrameData=np.array()
+        OneETF_mx=OneETFdf.as_matrix()  
+#         if ETF_no==690:
+#             ETF_no=ETF_no
+            
+        if len(OneETFdf) < data_days:
+            zero_m=np.zeros((data_days-len(OneETFdf),OneETF_mx.shape[1]))
+            OneETF_mx=np.insert(arr=OneETF_mx, obj=0, values=zero_m, axis=0)
+        OneETFFrameData=np.array(OneETFFrameData)
+     
+        OneETFFrameData=np.reshape(OneETFFrameData,(OneETFFrameData.shape[0],OneETFFrameData.shape[1],1))
+        
+        #y train data
+        y_train_list.append(OneETFFrameData[:int(number_train)])                           
+      
+        #y test data
+        y_test_list.append(OneETFFrameData[int(number_train):])
+
+   
+    return [x_train_list, y_train_list, x_test_list, y_test_list]
 
 def build_model(input_length,input_dim):
     d=0.3
@@ -97,7 +159,7 @@ def build_model(input_length,input_dim):
     model.add(Dropout(d))
 
     model.add(Dense(16,kernel_initializer="uniform",activation='relu'))
-    model.add(Dense(1,kernel_initializer="uniform",activation='linear'))
+    model.add(Dense(5,kernel_initializer="uniform",activation='linear'))
     
     model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
     
@@ -132,21 +194,19 @@ def TestGroup(dfs):
 # Main entry point
 #
 def main(argv=None):
-    tsharepdf=pd.read_csv('tsharepEnTitle.csv',encoding = 'big5',thousands=',',usecols=["No","Date","Open","High","Low","Close","Volume"],low_memory=False)  #nrows=100000,,verbose=True
-    tetfpdf=pd.read_csv('tetfpEnTitle.csv',encoding = 'big5',thousands=',',usecols=["No","Date","Open","High","Low","Close","Volume"],low_memory=False)  
-    
-    
-    tsharepdf_norm=normalize(tsharepdf)
-    tetfpdf_norm=normalize(tetfpdf)
+#     tsharepdf=pd.read_csv('tsharepEnTitle.csv',encoding = 'big5',thousands=',',usecols=["No","Date","Open","High","Low","Close","Volume"],low_memory=False)  #nrows=100000,,verbose=True
+#     tetfpdf=pd.read_csv('tetfpEnTitle.csv',encoding = 'big5',thousands=',',usecols=["No","Date","Open","High","Low","Close","Volume"],low_memory=False)  
+#     
+#     
+#     tsharepdf_norm=normalize(tsharepdf)
+#     tetfpdf_norm=normalize(tetfpdf)
     
     
     x_train, y_train, x_test, y_test = data_helper(tsharepdf,tetfpdf, 20)
-        
     
-    ''' 
-    model=build_model(20,5)
-    model.fit(x_train, y_train, batch_size=128,epochs=50,validation_split=0.1,verbose=1)
-    
+#     model=build_model(20,5)
+#     model.fit(x_train, y_train, batch_size=128,epochs=10,validation_split=0.1,verbose=1)
+    '''
     pred_train=model.predict(x_train)
     denorm_pred_train=denormalize(tsharepdf,pred_train)
     denorm_ytest_train=denormalize(tetfpdf,y_train)
@@ -172,7 +232,6 @@ def main(argv=None):
     #close.plot()
     #plt.plot(close)
     #plt.show()
-    '''
-
+'''
 if __name__ == '__main__':
     main()
