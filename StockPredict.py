c4c219b509b093cc856049886ca1c5912f86de7f
stanley1228 <stanleyfreeze@gmail.com>
2018-05-21 15:04:26 +0800

    add generate submission

--- StockPredict.py
+++ StockPredict.py
@@ -237,14 +237,139 @@ def TrainProcess():
     #close.plot()
     #plt.plot(close)
     #plt.show()
+def CompareList(list1,list2):
+    result=[1 if b>a else 0 if a==b else -1 for a,b in zip(list1,list2)]
+    return result
+
+def GenerateDataForRealTest(day_frame):
+    tsharepdf=pd.read_csv('tsharepEnTitle.csv',encoding = 'big5',thousands=',',usecols=["No","Date","Open","High","Low","Close","Volume"],low_memory=False)  #nrows=100000,,verbose=True
+    tetfpdf=pd.read_csv('tetfpEnTitle.csv',encoding = 'big5',thousands=',',usecols=["No","Date","Open","High","Low","Close","Volume"],low_memory=False)  
+ 
+    tsharepdf_norm=normalize(tsharepdf)
+    number_features=5
+    
+    print("=========================")
+    print("=====make input data=====")
+    print("=========================") 
+    StocksGroup=tsharepdf_norm.groupby('No') #find how many days of the data
+    Group1101=StocksGroup.get_group(1101)
+    data_days=len(Group1101)
+ 
+    x_test_list=[]
+    for stock_no,OneStockdf in StocksGroup:
+#         if stock_no==1108:
+#             break
+        if len(OneStockdf) != data_days:
+            continue
+        OneStockFrameData=[]
+        OneStock_mx=OneStockdf.as_matrix()
+        for index in range(data_days-day_frame,data_days-day_frame+1):
+            OneStockFrameData.append(OneStock_mx[index:index+day_frame,2:8]) #"No","Date","Name","Open","High","Low","Close","Volume"     
+        OneStockFrameData=np.array(OneStockFrameData)
+        OneStockFrameData=np.reshape(OneStockFrameData,(OneStockFrameData.shape[0],OneStockFrameData.shape[1],number_features))
+         
+        #number_train=round(0.5*OneStockFrameData.shape[0])
+ 
+        #x train data
+        #x_train_list.append(OneStockFrameData[:int(number_train)])   
+         
+        #x test data
+        x_test_list.append(OneStockFrameData)
+     
+    x_test_np_matrix_real_test=np.concatenate(x_test_list,axis=2) 
+    x_test_np_matrix_real_test.tofile('x_test_np_matrix_real_test.dat')
+    print('x_test_np_matrix_real_test=',end='')
+    print(x_test_np_matrix_real_test.shape)
+     
+    print("========================")
+    print("=====predict result=====")
+    print("========================") 
+    print('load stock_model_batch256.h5...',end='')
+    model = load_model('stock_model_batch256.h5')
+    print('Done')
+ 
+    print('pridict...',end='')
+    pred_y_result=model.predict(x_test_np_matrix_real_test)
+    print('Don')
+    denorm_pred_y_result=denormalize(tetfpdf,pred_y_result)
+    denorm_pred_y_result=np.reshape(denorm_pred_y_result,(18,5))
+    denorm_pred_y_result=denorm_pred_y_result.round(decimals=2)
+    print('denorm_pred_y_result.shape=',end='') 
+    print(denorm_pred_y_result.shape)
+    print('denorm_pred_y_result=') 
+    print(denorm_pred_y_result)
+    
+    #
+    #get newest close
+    #
+    
+#     print(tetfpdf[tetfpdf['Date'] == 20180504])
+#     ETF_close_20180504=tetfpdf[tetfpdf['Date'] == 20180504]['Close']
+    ETF_newest_df=tetfpdf.groupby('No').last()#last data of eahc company
+    #print(ETF_newest_df)
+    ETF_newest_close=ETF_newest_df['Close'].tolist()
+    print("ETF_newest_close=")
+    print(ETF_newest_close)
+    ETF_newest_date=tetfpdf.tail(1)['Date']
+    print("ETF_newest_date={0}".format(ETF_newest_date.iloc[0]))
+#     ETF_close_tail=ETF_close_tail.as_matrix()
+
+    '''
+    calculate up or down
+    compare to newest close to next dat
+    '''
+    print("==============================")
+    print("=====calculate up or down=====")
+    print("==============================")
+    print("list(denorm_pred_y_result[:,0]) monday=")
+    print(list(denorm_pred_y_result[:,0]))
+    Mon_ud=CompareList(ETF_newest_close,list(denorm_pred_y_result[:,0]))#denorm_pred_y_result #(18,5)
+    print("Mon_ud=")
+    print(Mon_ud)
+
+    Tue_ud=CompareList(list(denorm_pred_y_result[:,0]),list(denorm_pred_y_result[:,1]))#denorm_pred_y_result #(18,5)
+    Wed_ud=CompareList(list(denorm_pred_y_result[:,1]),list(denorm_pred_y_result[:,2]))#denorm_pred_y_result #(18,5)
+    Thu_ud=CompareList(list(denorm_pred_y_result[:,2]),list(denorm_pred_y_result[:,3]))#denorm_pred_y_result #(18,5)
+    Fri_ud=CompareList(list(denorm_pred_y_result[:,3]),list(denorm_pred_y_result[:,4]))#denorm_pred_y_result #(18,5)
+
+    print("Tue_ud=") 
+    print(Tue_ud)
+    print("Wed_ud=")
+    print(Wed_ud)
+    print("Thu_ud=")
+    print(Thu_ud)
+    print("Fri_ud=")
+    print(Fri_ud)
+    
+    print("======================")
+    print("=====write to csv=====")
+    print("======================")   
+    submitdf=pd.read_csv('Submission.csv',thousands=',',dtype={'ETFid':str} ,low_memory=False) 
+    print(submitdf.info())
+    submitdf['Mon_cprice']=list(denorm_pred_y_result[:,0])
+    submitdf['Tue_cprice']=list(denorm_pred_y_result[:,1])
+    submitdf['Wed_cprice']=list(denorm_pred_y_result[:,2])
+    submitdf['Thu_cprice']=list(denorm_pred_y_result[:,3])
+    submitdf['Fri_cprice']=list(denorm_pred_y_result[:,4])
+    submitdf['Mon_ud']=Mon_ud
+    submitdf['Tue_ud']=Tue_ud
+    submitdf['Wed_ud']=Wed_ud
+    submitdf['Thu_ud']=Thu_ud
+    submitdf['Fri_ud']=Fri_ud
+    submitdf.to_csv("myresult.csv",index=False,line_terminator='\r\n',float_format='%.2f')
+# 
+#     print(submitdf)
+    return 
 
 #
 # Main entry point
 #
 def main(argv=None):
     #Testpandas()
-    TrainProcess()
+#     TrainProcess()
     
+    GenerateDataForRealTest(20)
+  
 
 if __name__ == '__main__':
     main()
